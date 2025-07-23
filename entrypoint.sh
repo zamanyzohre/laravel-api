a191fba50d397099225607610918203528ec7033
zohre <zozo.tiktak@gmail.com>
2025-07-23 20:11:28 +0330

    update file entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
-# فقط یک بار key رو تولید کن اگر وجود نداره
-if [ ! -f /app/storage/oauth-private.key ]; then
+# اطمینان از وجود APP_KEY
+if [ ! -f /app/storage/app.key ]; then
+  echo "🔐 Generating app key and running migrations..."
   php artisan key:generate
   php artisan migrate --force
 fi
 
-# اجرای اصلی لاراول
+# لینک دادن storage/public به public/storage (برای نمایش فایل‌های آپلودی)
+echo "🔗 Running storage:link..."
+php artisan storage:link
+
+# کش کردن تنظیمات
+php artisan config:cache
+php artisan route:cache
+php artisan view:cache
+
+# اجرای Laravel
+echo "🚀 Starting Laravel server..."
 php artisan serve --host=0.0.0.0 --port=8000
